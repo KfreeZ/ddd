e962ebbd28dfd95a5a4059f65dca9573bbf12820
kfreez <pi@raspberrypi.(none)>
2014-11-10 11:36:21 +0000

    serial writing command finished

--- xdd.py
+++ xdd.py
@@ -68,21 +68,60 @@ class CmdGenerator:
     VARIABLEBYTE = 0x00
     #                               ---head---  ---addr----   -len- -cmd- ---crc pt1--- ---crc pt2--- -tail
     template_ResetRcvr = bytearray([0x7E, 0x3E, VARIABLEBYTE, 0x02, 0x01, VARIABLEBYTE, VARIABLEBYTE, 0x3C])
-    #                             ---head---  -ad- -len- -cmd-  ---yr pt1---  ---yr pt2---  ----month---  ----date----  ----hour----  ----min-----  ----sec----- ---crc pt1--- ---crc pt2--- -tail
+    #                             ---head---  -id- -len- -cmd-  ---yr pt1---  ---yr pt2---  ----month---  ----date----  ----hour----  ----min-----  ----sec----- ---crc pt1--- ---crc pt2--- -tail
     template_SetTime = bytearray([0x7E, 0x3E, 0xFF, 0x09, 0x10, VARIABLEBYTE, VARIABLEBYTE, VARIABLEBYTE, VARIABLEBYTE, VARIABLEBYTE, VARIABLEBYTE, VARIABLEBYTE, VARIABLEBYTE, VARIABLEBYTE, 0x3C])
-    #                               ---head---  -----addr----  -len- -cmd- ---crc pt1--- ---crc pt2-- -tail
-    template_QueryCards = bytearray([0x7E, 0x3E, VARIABLEBYTE, 0x05, 0x01, VARIABLEBYTE, VARIABLEBYTE, 0x3C])
+    #                               ---head---  -----id-----  -len- -cmd- --prev addr-- ---prev sn---  ----rsrv----  ---crc pt1--- ---crc pt2-- -tail
+    template_QueryCards = bytearray([0x7E, 0x3E, VARIABLEBYTE, 0x05, 0x01, VARIABLEBYTE, VARIABLEBYTE, VARIABLEBYTE, VARIABLEBYTE, VARIABLEBYTE, 0x3C])
 
     def calcCrc(self, bytearray):
         retArray = [0, 0]
         crcValue = crc16.crc16xmodem(buffer(bytearray))
-        crcByteArray = struct.unpack("2b", struct.pack("H", crcValue))
-        retArray[0] = crcByteArray[1]
-        retArray[1] = crcByteArray[0]
-        return retArray
+        crcByteArray = struct.unpack("2B", struct.pack("H", crcValue))
+        return crcByteArray
+
+    def getTimebyteArray(self):
+        now = time.localtime(time.time())
+        yearArray = struct.unpack("2B", struct.pack("H", now.tm_year))
+        timeByteArray = bytearray(7)
+        timeByteArray[0] = yearArray[1]
+        timeByteArray[1] = yearArray[0]
+        timeByteArray[2] = now.tm_mon
+        timeByteArray[3] = now.tm_mday
+        timeByteArray[4] = now.tm_hour
+        timeByteArray[5] = now.tm_min
+        timeByteArray[6] = now.tm_sec
+        return timeByteArray
 
     def gnrtRstCmd(self, devId):
-        
+        resetCmd = template_ResetRcvr  
+        resetCmd[2] = devId
+        cmdBody = bytearray(resetCmd[2], resetCmd[3], resetCmd[4])
+        crcArray = self.calcCrc(cmdBody)
+        # attention: reverse the sequence
+        ResetCmd[5] = crcArray[1]
+        ResetCmd[6] = crcArray[0]
+
+    def gnrtQueryCmd(self, devId, prevAddr, prevSn):
+        queryCmd = template_ResetRcvr  
+        queryCmd[2] = devId
+        queryCmd[5] = prevAddr
+        queryCmd[6] = prevAddr
+        cmdBody = bytearray(queryCmd[2], queryCmd[3], queryCmd[4], queryCmd[5], queryCmd[6], queryCmd[7], queryCmd[8], queryCmd[9], queryCmd[10], queryCmd[11])
+        crcArray = self.calcCrc(cmdBody)
+        # attention: reverse the sequence
+        queryCmd[8] = crcArray[1]
+        queryCmd[9] = crcArray[0]
+
+    def gnrtSetTimeCmd(self, devId):
+        setTimeCmd = template_SetTime
+        timeByteArray = self.getTimebyteArray()
+        for x in xrange(0, 7):
+            setTimeCmd[5+x] = timeByteArray[x]
+        cmdBody = bytearray(queryCmd[2], queryCmd[3], queryCmd[4], queryCmd[5], queryCmd[6], queryCmd[7])
+        crcArray = self.calcCrc(cmdBody)
+        # attention: reverse the sequence
+        setTimeCmd[12] = crcArray[1]
+        setTimeCmd[13] = crcArray[0]
 
 class CardManager:
     sIO = None
@@ -90,17 +129,17 @@ class CardManager:
         sIO = SerialIO()
         cmdGnrtr = CmdGenerator();
 
-    def initCardRcvr(self, addr):
-        initCmd = CmdGenerator.template_ResetRcvr
-        initCmd[2] = addr
-        cmdGnrtr.gnrtRstCmd(initCmd)
-        sIO.writeSerial(initCmd)
+    def initCardRcvr(self, devId):
+        rstCmd = cmdGnrtr.gnrtRstCmd(addr)
+        sIO.writeSerial(rstCmd)
 
-    def setCardRcvrTime(self):
-        print "set time to card receiver"
+    def setCardRcvrTime(self, devId):
+        setTimeCmd = cmdGnrtr.gnrtSetTimeCmd(devId)
+        sIO.writeSerial(setTimeCmd)
 
-    def readAllCards(self, rcvrId):
-        print "first read from receiver"
+    def readAllCards(self, devId):
+        queryCmd = cmdGnrtr.gnrtQueryCmd(devId, 0xFF, 0x00)
+        sIO.writeSerial(queryCmd)
 
     def saveResults(self):
         print "save results"
