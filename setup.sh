25e35bbc66bb0d5d6694789d6c22f673edfe5482
Cliff <kfreec@gmail.com>
2015-01-09 12:53:14 +0800

    Update setup.sh
    change to absolute paths

--- setup.sh
+++ setup.sh
@@ -3,11 +3,10 @@
 sudo apt-get install python-dev
 
 sudo tar -xzf crc16-0.1.1.tar.gz
-cd ~/ddd/crc16-0.1.1
+cd /home/pi/ddd/crc16-0.1.1
 sudo python setup.py build
 sudo python setup.py install
 
-cd ../
 sudo apt-get install python-pip
 
 pip install bitstring
@@ -18,4 +17,4 @@ sudo apt-get install samba samba-common-bin
 
 sudo smbpasswd -a ddd
 
-sudo cp ~/ddd/smb.conf /etc/samba/smb.conf
+sudo cp /home/pi/ddd/smb.conf /etc/samba/smb.conf
