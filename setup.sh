9971d5cab7093d31435792438b44371bf7490bad
Cliff <kfreec@gmail.com>
2015-01-09 12:46:23 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,15 @@ cd ~/ddd/crc16-0.1.1
 sudo python setup.py build
 sudo python setup.py install
 
+cd ../
+sudo apt-get install python-pip
+
 pip install bitstring
 
 pip install requests
 
 sudo apt-get install samba samba-common-bin
 
-sudo sambapasswd -a ddd
+sudo smbpasswd -a ddd
 
 sudo cp ~/ddd/smb.conf /etc/samba/smb.conf
